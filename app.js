6c23a47a72730f2e1dd1512b958f2de0d98cd9a2
Claudiane4 <claudiane.dias1332@gmail.com>
2025-08-03 09:07:38 -0400

    adicionado a função adicionar na lista

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+let lista = [];
 let amigos = [];
+
+function exibirTextoTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTextoTela('h2','Digite o nome de 10 amigos');
+
+function sortearAmigo(){
+   
+}
 
 function adicionarAmigo(){
     let campo = document.querySelector('input').value;
-    if(campo == ''){
-        alert('Por favor, insira um nome');
-    } else {
+    if(campo != '' ){
         amigos.push(campo);
-        console.log(campo);
+    } else {
+        alert('Por favor, insira um nome'); 
     }
     limparCampo();
+    adicionaLista();
 }
+
+
+function adicionaLista(){
+    lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++){
+        let nome = amigos[i];
+        let el = document.createElement('li');
+        el.appendChild(document.createTextNode(nome));
+        lista.appendChild(el);
+    }
+}
+
 function limparCampo(){
     campo = document.querySelector('input');
     campo.value = '';
 }
-
