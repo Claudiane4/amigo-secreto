28a011a10e505f01c53e06f311c1415f8afdb668
claudiane <claudiane.dias1332@gmail.com>
2025-08-05 16:11:15 -0400

    adicionado gerar nomes aleatórios

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let lista = [];
 let amigos = [];
 
@@ -5,23 +6,32 @@ function exibirTextoTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-exibirTextoTela('h2','Digite o nome de 10 amigos');
+exibirTextoTela('h2','Digite o nome de amigos');
 
 function sortearAmigo(){
-   
+   if(amigos.length < 1){
+     alert('Não há nomes na lista');
+   }else{
+        let indiceAleatorio = parseInt(Math.random() * amigos.length + 1);
+        let amigoAleatorio = amigos[indiceAleatorio];
+        let resultadoAmigo = document.getElementById('resultado');
+        resultadoAmigo.innerHTML = amigoAleatorio;
+   }
 }
 
 function adicionarAmigo(){
     let campo = document.querySelector('input').value;
-    if(campo != '' ){
-        amigos.push(campo);
-    } else {
-        alert('Por favor, insira um nome'); 
-    }
+    validarEntrada(campo, amigos);
     limparCampo();
     adicionaLista();
 }
-
+function validarEntrada(entrada, nomeamigo){
+    if(entrada == ''){
+        alert('Por favor, insira um nome');
+    }else {
+            nomeamigo.push(entrada.trimStart());
+    }
+}
 
 function adicionaLista(){
     lista = document.getElementById('listaAmigos');
